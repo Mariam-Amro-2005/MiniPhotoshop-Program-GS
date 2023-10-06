997a2c5ba21b77ce97d13ba9e484ba60423952b6
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2023-10-06 19:10:43 +0300

    Committing my first two filters: The invertion filter and the rotation filter. These filters are for grayscaled bmp images only.

---
+++ InvertionFilter.cpp
@@ -0,0 +1,74 @@
+// Program: demo2.cpp
+// Purpose: Demonstrate use of bmplip for handling
+//          bmp colored and grayscale images
+//          Program load a gray image and store in another file
+// Author:  Mohammad El-Ramly
+// Date:    30 March 2018
+// Version: 1.0
+
+// FCAI – OOP Programming – 2023 - Assignment 1 
+// Program Name:				xxxxxx.cpp
+// Last Modification Date:	xx/xx/xxxx
+// Author1 and ID and Group:	xxxxx xxxxx
+// Author2 and ID and Group:	xxxxx xxxxx
+// Author3 and ID and Group:	xxxxx xxxxx
+// Teaching Assistant:		xxxxx xxxxx
+// Purpose:..........	
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+
+using namespace std;
+unsigned char image[SIZE][SIZE];
+
+void loadImage ();
+void saveImage ();
+void invertImage();
+
+int main()
+{
+  loadImage();
+  invertImage();
+  saveImage();
+  return 0;
+}
+
+//_________________________________________
+void loadImage () {
+   char imageFileName[100];
+
+   // Get gray scale image file name
+   cout << "Enter the source image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   readGSBMP(imageFileName, image);
+}
+
+//_________________________________________
+void saveImage () {
+   char imageFileName[100];
+
+   // Get gray scale image target file name
+   cout << "Enter the target image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   writeGSBMP(imageFileName, image);
+}
+
+//_________________________________________
+void invertImage() {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j< SIZE; j++) {
+
+        image[i][j] = 255 - image[i][j]; // Subtracting original value from 255 to get inverse colour
+    }
+    
+  }
+}
---
+++ Menu.cpp
@@ -0,0 +1,138 @@
+// FCAI – OOP Programming – 2023 - Assignment 1 
+// Program Name:				RotatingFilter.cpp
+// Last Modification Date:	6/10/2023
+// Author1 and ID and Group:	xxxxx xxxxx
+// Author2 and ID and Group:	Mariam Amro 20221217
+// Author3 and ID and Group:	xxxxx xxxxx
+// Teaching Assistant:		xxxxx xxxxx
+// Purpose: This is the main program that edits photos
+
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+
+void loadImage ();
+char Options();
+void saveImage();
+void invertImage();
+void rotateImage();
+
+using namespace std;
+unsigned char image[SIZE][SIZE];
+
+int main(){
+    cout << "Welcome User! \n";
+    loadImage();
+    string operation, exit;
+    operation = "1";
+    exit = "0";
+    while(operation != exit){
+        operation = Options();
+        cout << operation << "\n";
+    }
+
+}
+
+void loadImage () {
+   char imageFileName[100];
+   // Get gray scale image file name
+   cout << "Please enter file name of the image to process: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   readGSBMP(imageFileName, image);
+}
+
+char Options(){
+    string option;
+    string line;
+    fstream menu("Menu.txt");  // Menu.txt contains the options of filters to choose from
+    while(getline(menu, line)){   //This reads the menu file and displays it
+        cout << line << "\n";
+
+    }
+    menu.close();
+    bool invalid = 0;
+    cin >> option;
+    do
+    {
+        switch (option[0])    //To allow users to choose options and prevent entering of invalid options
+    {
+        case "1":
+            //
+            break;
+        case "2":
+            invertImage();
+            break;
+        case "3":
+            //
+            break;
+        case "4":
+            //
+            break;
+        case "5":
+            rotateImage();
+            break;
+        case "6":
+            //
+            break;
+        case "7":
+            //
+            break;
+        case "8":
+            //
+            break;
+        case "9":
+            //
+            break;
+        case "a":
+            //
+            break;
+        case "b":
+            //
+            break;
+        case "c":
+            //
+            break;
+        case "d":
+            //
+            break;
+        case "e":
+            //
+            break;
+        case "f":
+            //
+            break;
+        case "s":
+            saveImage();
+            break;
+        case "0":
+            break;
+    
+    default:
+        invalid = 1;
+        cout << "Sorry! Invalid option. Please choose a correct option.\n"
+        break;
+    }
+        
+    } while (invalid);
+    
+    return option;
+
+}
+
+void saveImage() {
+   char imageFileName[100];
+
+   // Get gray scale image target file name
+   cout << "Enter the target image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   writeGSBMP(imageFileName, image);
+}
