bfe8097d62098986a541e5b0f3e1c61329620da4
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2023-10-09 01:07:37 +0300

    add functions

--- Menu.cpp
+++ Menu.cpp
@@ -157,7 +157,7 @@ void enlargeImage(){
     cin >> quarter;
     x = y = 0;
     if (quarter == 4)       //Assigning a value to x and y to adjust the for 
-    {                       //loop according to quarter choosen
+    {                       //loop according to quarter chosen
         x = y = 128;
     }else if (quarter == 3)
     {
@@ -175,6 +175,48 @@ void enlargeImage(){
                                                                 // and two bits in adjacent columns.
         }
         
+    }
+
+}
+
+void invertImage() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j< SIZE; j++) {
+
+            image[i][j] = 255 - image[i][j]; // Subtracting original value from 255 to get inverse colour
+        }
+
+    }
+}
+
+void rotateImage() {
+    int temp, degree;
+    string direction, compare;
+    compare = "anticlockwise";
+    cout << "Please choose which degree of rotation: ";
+    cin >> degree;
+    cout << "Please choose the direction of rotation - (c)lockwise or (a)nticlockwise: "; // anticlockwise in x degrees is the same as clockwise (360 - x) degrees
+    cin >> direction;
+    if (direction[0] == compare[0])
+    {
+        degree = 360 - degree;
+    }
+    for (int k = 0; k < (degree/90); k++)
+    {
+        for(int i = 0; i < SIZE ; ++i){   // This mirrors the array from the middle to change the direction of the principal diagonal to become the off diagonal
+            for(int j = 0; j <SIZE/2; ++j){  //Also make it easier to swap cell contents
+                temp = image[i][j];
+                image[i][j] = image[i][SIZE-j-1];
+                image[i][SIZE-j-1] = temp;
+            }
+        }
+        for(int i = 0; i < SIZE ; ++i){   // This swaps the cell contents into their final place
+            for(int j = 0; j < SIZE-i-1; ++j){
+                temp = image[i][j];
+                image[i][j] = image[SIZE-j-1][SIZE-i-1];
+                image[SIZE-j-1][SIZE-i-1] = temp;
+            }
+        }
     }
 
 }
