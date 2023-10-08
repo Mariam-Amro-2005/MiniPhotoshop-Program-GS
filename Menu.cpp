7d10718c8b3ae5e95295d58e0ef546038e651a26
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2023-10-08 23:42:15 +0300

    Integrated enlarging filter into main program

--- Menu.cpp
+++ Menu.cpp
@@ -19,9 +19,12 @@ string Options();
 void saveImage();
 void invertImage();
 void rotateImage();
+void enlargeImage();
+void copyImage();
 
 using namespace std;
 unsigned char image[SIZE][SIZE];
+unsigned char image2[SIZE][SIZE];
 
 int main(){
     cout << "Welcome User! \n";
@@ -133,4 +136,45 @@ void saveImage() {
    // Add to it .bmp extension and load image
    strcat (imageFileName, ".bmp");
    writeGSBMP(imageFileName, image);
+}
+
+
+void copyImage(){       //This is to perserve the original data
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            image2[i][j] = image[i][j];
+        }
+        
+    }
+    
+}
+
+void enlargeImage(){
+    int quarter, x, y;
+    cout << "Which quarter would you like to enlarge - 1, 2, 3, or 4 ? ";
+    cin >> quarter;
+    x = y = 0;
+    if (quarter == 4)       //Assigning a value to x and y to adjust the for 
+    {                       //loop according to quarter choosen
+        x = y = 128;
+    }else if (quarter == 3)
+    {
+        x = 128;
+    }else if (quarter == 2)
+    {
+        y = 128;
+    }
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            image[i][j] = image2[int(i/2) + x][int(j/2) + y];   //This assigns each cell/bit in the chosen quarter
+                                                                //four bits in the original image; two bits in a row
+                                                                // and two bits in adjacent columns.
+        }
+        
+    }
+
 }
